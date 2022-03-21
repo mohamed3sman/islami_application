c541da749e9c3de17ffe5f22867ce53be2cee70e
Mohamed Osman <77281221+mohamed3sman@users.noreply.github.com>
2022-03-21 11:57:16 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/home_screen.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Islami",
+      routes:
+      {
+        HomeScreen.routeName : (buildContext)=>HomeScreen()
+      },
+      initialRoute: HomeScreen.routeName,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
